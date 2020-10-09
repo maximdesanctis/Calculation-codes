11b22a28099cc6110cf8a829060d59ee510a494c
maxobaerchen <71792292+maxobaerchen@users.noreply.github.com>
2020-10-09 21:02:34 +0200

    Update Square root calculator.py

--- Square root calculator.py
+++ Square root calculator.py
@@ -4,8 +4,8 @@ a = float(input("Enter the number from which you want the square root.."))
 b = float(input("Enter the starting value for the iterations.."))
 c = int(input("Enter the number of iterations you want to run.."))
 
-print(a)
-for i in range(c):
-    b = (1/2)*(b+a/b)
-    print(b)
-print("The square root is approximately " + str(b) + ".")
+if __name__ == "__main__":
+    for i in range(c):
+        b = (1/2)*(b+a/b)
+        print(b)
+    print("The square root is approximately " + str(b) + ".")
